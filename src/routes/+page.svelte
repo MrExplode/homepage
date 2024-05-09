b3e2d12b8ae9f50dd2c96864ece9f3fd461b0cf8
MrExplode <me@sunstorm.rocks>
2024-05-09 16:47:20 +0200

    feat: base layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import avatar from '$lib/images/avatar.png'
+</script>
+
+<div class="container flex h-screen w-screen flex-col justify-center">
+    <div class="flex items-center gap-8">
+        <img src={avatar} alt="avatar" class="h-32 w-32 rounded-full" />
+        <div class="flex flex-col gap-4">
+            <h1 class="text-4xl font-bold tracking-tight">SunStorm</h1>
+            <p class="text-muted-foreground">
+                21 year old <span class="font-bold">software developer</span>
+            </p>
+        </div>
+    </div>
+</div>
