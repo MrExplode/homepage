fce5a02a59899d917b87ca06640e2bc6a2b97dcb
MrExplode <me@sunstorm.rocks>
2025-01-29 18:28:17 +0100

    feat: meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,10 +4,30 @@
     import 'lenis/dist/lenis.css'
 
     let { children } = $props()
+
+    const description =
+        '22 year old undergraduate Full Stack Developer, DevOps enjoyer, occasionally drummer and light designer.'
+    const icon = 'https://sunstorm.rocks/icon_256.png'
 </script>
 
 <svelte:head>
     <title>SunStorm</title>
+    <link rel="icon" type="image/png" href="/icon_256.png" />
+    <meta name="description" content={description} />
+    <meta name="theme-color" content="#616161" />
+    <meta name="googlebot" content="notranslate" />
+    <link rel="canonical" href="https://sunstorm.rocks" />
+
+    <meta property="org:type" content="website" />
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:title" content="SunStorm" />
+    <meta property="og:description" content={description} />
+    <meta property="og:image" content={icon} />
+
+    <meta property="twitter:title" content="SunStorm" />
+    <meta property="twitter:description" content={description} />
+    <meta property="twitter:card" content="summary" />
+    <meta property="twitter:image" content={icon} />
 </svelte:head>
 
 <ThemeSwitch />
